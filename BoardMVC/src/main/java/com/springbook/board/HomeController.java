91c658e4f45df4b7d8783053327ac8eb320af060
Administrator <Administrator@APC-4>
2020-07-21 17:06:33 +0900

    2020/07/21-2

--- BoardMVC/src/main/java/com/springbook/board/HomeController.java
+++ BoardMVC/src/main/java/com/springbook/board/HomeController.java
@@ -6,22 +6,27 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-/**
- * Handles requests for the application home page.
- */
+// controller 애노테이션을 주면 빈에 자동 등록됨
+// 핸들러 맵핑이 자동관리 함
 @Controller
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
+	@Autowired
+	private DataDAO dao;
+	
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		dao.getData();
+		return "login";
+	}
+	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
